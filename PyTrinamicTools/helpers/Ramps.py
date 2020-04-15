c4b58a105fb49fe229f407188e7c8a621586bbec
bp <partzsch@trinamic.com>
2020-04-15 09:10:37 +0200

    Add: New Module for ramp functions.
    Including an implementation for a linear velocity ramp.

---
+++ PyTrinamicTools/helpers/Ramps.py
@@ -0,0 +1,65 @@
+"""Module to run different ramp motions.
+
+Please note that thees functions work in blocking mode.
+"""
+
+import time
+
+
+class VelocityRampRunner:
+    """Class to run velocity ramps."""
+
+    def __init__(self, velocity_update_callback, update_cycle_time_ms=0):
+        """If update_cycle_time_ms is set to 0, the velocity is updated as fast as possible.
+
+        You need to give a function that updates the velocity.
+        """
+        self._velocity_update_callback = velocity_update_callback
+        self._update_cycle_time_ms = update_cycle_time_ms
+
+    def run_linear_ramp(self, start_velocity_rpm, target_velocity_rpm, time_delta_ms):
+        """Update the velocity on a linear basis from start_velocity to the target_velocity within the given time."""
+        if self._update_cycle_time_ms:
+            self._velocity_ramp_fixed_cycle(start_velocity_rpm, target_velocity_rpm, time_delta_ms)
+        else:
+            self._velocity_ramp_fast(start_velocity_rpm, target_velocity_rpm, time_delta_ms)
+
+    def _velocity_ramp_fixed_cycle(self, start_velocity_rpm, target_velocity_rpm, time_delta_ms):
+        """Sub function that updates the velocity on a update_cycle_time_ms basis.
+
+        Note that if time_delta_ms can not be divided by update_cycle_time_ms,
+        the linear ramp will be longer or shorter than expected.
+        """
+        update_cycles = round(time_delta_ms / self._update_cycle_time_ms)
+        acceleration = (target_velocity_rpm - start_velocity_rpm) / time_delta_ms
+        for i in range(update_cycles):
+            start_time = self._time_ms()
+            velocity_update = acceleration*(i*self._update_cycle_time_ms) + start_velocity_rpm
+            self._velocity_update_callback(int(velocity_update))
+            stop_time = self._time_ms()
+            delay_time_ms = (stop_time - start_time)
+            if delay_time_ms < self._update_cycle_time_ms:
+                time.sleep((self._update_cycle_time_ms-delay_time_ms)/1000)
+        # always set the target_velocity_rpm at the end
+        self._velocity_update_callback(int(target_velocity_rpm))
+
+    def _velocity_ramp_fast(self, start_velocity_rpm, target_velocity_rpm, time_delta_ms):
+        """Sub function that updates the velocity as fast as possible.
+
+        Remark: Seems to update the velocity on a 1/4 ms basis on my machine
+        """
+        start_time = self._time_ms()
+        acceleration = (target_velocity_rpm - start_velocity_rpm) / time_delta_ms
+        self._velocity_update_callback(start_velocity_rpm)
+        stop_time = self._time_ms()
+        delay_time_ms = (stop_time - start_time)
+        while delay_time_ms < time_delta_ms:
+            velocity_update = acceleration*delay_time_ms + start_velocity_rpm
+            self._velocity_update_callback(int(velocity_update))
+            stop_time = self._time_ms()
+            delay_time_ms = (stop_time - start_time)
+        self._velocity_update_callback(target_velocity_rpm)
+
+    @classmethod
+    def _time_ms(cls):
+        return time.perf_counter() * 1000
